72721274d52e60631388e7148ac240f13dd0de90
sanjayshrestha <sanjayshrestha772@gmail.com>
2025-10-16 10:00:05 +0545

    type error fix

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,42 +1,41 @@
-import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action'
-import { Metadata } from 'next'
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import React from 'react'
+import React, { JSX } from 'react';
 import ProfilePageClient from './ProfilePageClient';
 
 interface PageProps {
-  params: {
-    username: string
-  }
+  params:Promise<{username:string}>
 }
 
-export const generateMetadata=async({params}:PageProps):Promise<Metadata>=>{
-  const username=await params.username
-  const user=await getProfileByUsername(username);
+export const generateMetadata = async ({ params }: PageProps): Promise<Metadata> => {
+  const username = (await params).username;
+  const user = await getProfileByUsername(username);
 
-  
   return {
-    title:`${user?.name ?? user?.username}`
-  }
-}
+    title: `${user?.name ?? user?.username}`
+  };
+};
 
-const ProfilePage =async ({params}:PageProps) => {
-    const user=await getProfileByUsername(params.username);
-  if(!user) notFound()
+const ProfilePage = async ({ params }: PageProps): Promise<JSX.Element> => {
+  const username = (await params).username;
+  const user = await getProfileByUsername(username);
+  if (!user) notFound();
 
-    const [posts,likedPosts,isCurrentUserFollowing]=await Promise.all([
-      getUserPosts(user.id),
-      getUserLikedPosts(user.id),
-      isFollowing(user.id)
-    ])
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id)
+  ]);
+
   return (
-   <ProfilePageClient
-   user={user}
-   posts={posts}
-   likedPosts={likedPosts}
-   isFollowing={isCurrentUserFollowing}
-   />
-  )
-}
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
